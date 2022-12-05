eaa895696698fe8ee4ceaa47160720286d1f5371
Manu <le.marques.emanuel@gmail.com>
2022-12-05 11:14:00 +0100

    added toggle navbar hidden

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,10 +11,15 @@ export default function Header() {
         };
 
         useEffect(() => {
+          const menu = document.getElementById('navbar-hamburger');
             if (open) {
-               document.getElementsByClassName('open-menu').click()
+             document.getElementById('navbar-hamburger').click(menu.classList.remove('hidden'));
             }
-        }, [open]);
+            else {
+              document.getElementById('navbar-hamburger').click(menu.classList.add('hidden'));
+            }
+        }, [open])
+        console.log('statut', open);
            
 
 
@@ -26,7 +31,7 @@ export default function Header() {
     <a href="#" class="flex items-center">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
     </a>
-    <button onClick={toggleMenu} data-collapse-toggle="navbar-hamburger" type="button" class="open-button inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+    <button onClick={toggleMenu} data-collapse-toggle="navbar-hamburger" type="button" class=" inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
       <span class="sr-only">Open main menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
