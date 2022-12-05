f8628769fd4bcd137484d9b28eb5ac93934149ad
Manu <le.marques.emanuel@gmail.com>
2022-12-05 16:08:41 +0100

    added more design

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -24,10 +24,9 @@ export default function Header() {
 <nav class="p-3 border-gray-200  bg-gray-50 dark:bg-red-700 dark:border-gray-700">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <a href="#" class="flex items-center">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Pokemon Finder</span>
+        <span class="italic self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Pokemon Finder</span>
     </a>
     <button onClick={toggleMenu} data-collapse-toggle="navbar-hamburger" type="button" class=" inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-50 dark:hover:bg-yellow-400 dark:focus:ring-gray-900" aria-controls="navbar-hamburger" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
     <div class="hidden w-full" id="navbar-hamburger">
