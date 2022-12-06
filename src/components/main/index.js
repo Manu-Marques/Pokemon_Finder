4559be8f1a9804a98878ddd582693482fb9d8257
Manu <le.marques.emanuel@gmail.com>
2022-12-06 22:30:43 +0100

    added content for main/header and footer

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,9 +1,8 @@
 
 export default function Main() {
     return (
-        <main className="text-center text-4xl font-bold underline">
+        <div className="h-full w- text-center text-4xl font-bold underline">
         <h1>My Main</h1>
-
-        </main>
+        </div>
     );
     }
