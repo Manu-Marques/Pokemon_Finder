97fd120dbf47efa1fce6b1a8b2dc22a649b9c1f5
Manu <le.marques.emanuel@gmail.com>
2022-12-14 16:31:32 +0100

    added details card

--- src/components/detailsCard/index.js
+++ src/components/detailsCard/index.js
@@ -1,8 +1,17 @@
+import evoli from "../../../src/assets/evoli.jpg";
+
+
 export default function detailsCard() {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1>Hello</h1>
-            </div>
+       
+<div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img src={evoli} class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</div>
+
             )
         }
     
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,7 +2,7 @@ import pikachu from "../../../src/assets/pikachu.jpeg";
 import evoli from "../../../src/assets/evoli.jpg";
 import dracaufeu from "../../../src/assets/dracaufeu.png";
 
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Slide } from "react-slideshow-image";
 import 'react-slideshow-image/dist/styles.css'
@@ -37,10 +37,10 @@ export default function Home() {
     <span class="text-base font-bold text-red-700">Attaque</span> : Lightning-rod
     </li>
 </ul>
-        <a href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Plus de
+        <Link to="/details-card" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Plus de détails
             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
+        </Link>
     </div>
     {/* end 1 card */}
     </div>
