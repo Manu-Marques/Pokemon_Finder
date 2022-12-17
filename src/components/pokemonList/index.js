6ae710e9209162a4579502723862de04d5a8095d
Manu <le.marques.emanuel@gmail.com>
2022-12-17 16:29:35 +0100

    starting api call

--- src/components/pokemonList/index.js
+++ src/components/pokemonList/index.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import evoli from "../../../src/assets/evoli.jpg";
 
-export default function PokemonList() {
+export default function PokemonList({pokemon}) {
     return (
         <div class="flex flex-wrap">
             <figure class="max-w-lg">
+              <Link to="/pokemon/1">
               <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
-              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+              </Link>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">{pokemon}</figcaption>
             </figure>
             <figure class="max-w-lg">
               <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
