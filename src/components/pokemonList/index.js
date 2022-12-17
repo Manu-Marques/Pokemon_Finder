2477e52a40718dd3573faa591301f873ff653ee1
Manu <le.marques.emanuel@gmail.com>
2022-12-17 13:54:47 +0100

    finished pokemon list for now

--- src/components/pokemonList/index.js
+++ src/components/pokemonList/index.js
@@ -2,15 +2,36 @@ import evoli from "../../../src/assets/evoli.jpg";
 
 export default function PokemonList() {
     return (
-        <div>
+        <div class="flex flex-wrap">
             <figure class="max-w-lg">
-              <img class="object-left-top max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
               <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
             </figure>
             <figure class="max-w-lg">
-              <img class="max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
               <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
             </figure>
+            <figure class="max-w-lg">
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+            </figure>
+            <figure class="max-w-lg">
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+            </figure>
+            <figure class="max-w-lg">
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+            </figure>            <figure class="max-w-lg">
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+            </figure>
+            <figure class="max-w-lg">
+              <img class="pr-1 max-w-96 h-auto rounded-lg" src={evoli} alt="image description"/>
+              <figcaption class="mt-2 text-2xl text-center text-gray-500 dark:text-gray-900">Evoli N°1</figcaption>
+            </figure>
+
+
         </div>
     )
 }
