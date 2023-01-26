d82aa9e07f25cd5de15511943afacd775669b9ca
Manu <le.marques.emanuel@gmail.com>
2023-01-26 10:31:07 +0100

    fix

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,7 +1,8 @@
 export default function Loading() {
     return (
-        <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        <div className="flex justify-center flex-col items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-8 border-yellow-400"></div>
+        <h1 className="pt-4 font-mono text-2xl text-center">Chargement des pokemons...</h1>
         </div>
     )
     }
